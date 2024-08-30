56885692a232d237b2a8417f64060c8d18b124ce
Rokon <rokon.zaman@pst.ag>
2024-08-31 00:13:43 +0200

    sdfas

--- tilesets/Chair.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Chairs" tilewidth="32" tileheight="32" tilecount="1683" columns="51">
- <image source="../../PST-village/tilesets/chairs.png" trans="ff00ff" width="1632" height="1080"/>
-</tileset>
